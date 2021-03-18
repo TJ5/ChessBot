ee7e0777acb23e5b3ec750d8045ba922ed947c13
TJ5__ <tejesh.anand@hotmail.com>
2021-03-17 18:56:22 -0700

    naive minimax

--- movetree.py
+++ movetree.py
@@ -4,12 +4,14 @@ from bot import Bot
 import copy
 class MoveTreeNode():
     #Move Tree node
-    def __init__(self, board: BoardWrapper, movesahead: int, maxdepth: int):
+    def __init__(self, board: BoardWrapper, movesahead: int, maxdepth: int, piececolor):
         
         self.board = board.getcopy()
         self.children = []
         self.maxdepth = maxdepth
         self.movesahead = movesahead
+        self.piececolor = piececolor
+        
         if (movesahead < maxdepth):
             self.addchildren()
     def addchildren(self):
@@ -22,7 +24,7 @@ class MoveTreeNode():
             #set childboard's board field to a shallow copy of the parent boardwrapper's board field
             childboard.board = self.board.board.copy() 
             childboard.pushmove(moves[i])
-            self.children.append(MoveTreeNode(childboard, self.movesahead + 1, self.maxdepth))
+            self.children.append(MoveTreeNode(childboard, self.movesahead + 1, self.maxdepth, self.piececolor))
             
             i = i + 1
         
@@ -46,3 +48,36 @@ class MoveTreeNode():
                 counter = counter + self.children[i].size()
                 i = i + 1
         return counter
+
+    def getbestboard(self):
+        #basecase
+        if (len(self.children) == 0):
+            return self.board
+        else:
+            if (self.movesahead % 2 == 0): 
+                i = 0
+                maxchild = None
+                maxeval = -20000
+                while (i < len(self.children)):
+                    if (self.children[i].getbestboard().shalloweval(self.piececolor) > maxeval):
+                        maxeval = self.children[i].getbestboard().shalloweval(self.piececolor)
+                        maxchild = self.children[i].getbestboard()
+                    i = i + 1
+                return maxchild
+            elif (self.movesahead % 2 == 1):
+                i = 0
+                minchild = None
+                mineval = 20000
+                while (i < len(self.children)):
+                    if (self.children[i].getbestboard().shalloweval(self.piececolor) < mineval):
+                        mineval = self.children[i].getbestboard().shalloweval(self.piececolor)
+                        minchild = self.children[i].getbestboard()
+                    i = i + 1
+                return minchild
+    def getbestmove(self):
+        bestboard = self.getbestboard()
+        movestack = bestboard.getmovestack()
+        #print(str(movestack))
+        move = movestack[-1 * self.maxdepth]
+        
+        return chess.Move.uci(move)
